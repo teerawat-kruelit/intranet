ec7c2ef169dd5abc89a8265f91b6d28e0a4e3d69
teerawat-k <84431883+teerawat-k@users.noreply.github.com>
2022-07-11 05:48:37 +0700

    add search on table

--- client/src/components/table.js
+++ client/src/components/table.js
@@ -1,33 +1,75 @@
-import React from 'react'
-import { Table } from "antd"
-import styled from 'styled-components'
+import { useState } from "react";
+import { Table as AntdTable } from "antd";
+import { Input } from "antd";
+import styled from "styled-components";
+import { useEffect } from "react";
 
-const TableConponent = styled(Table)`
-    .ant-table-tbody > tr > td,
-    .ant-table-thead > tr > th{
-        /* border: 1px solid gray; */
-        font-size: 10px;
+const TableComponent = styled(AntdTable)`
+  .ant-table-tbody > tr > td,
+  .ant-table-thead > tr > th {
+    /* border: 1px solid gray; */
+    font-size: 10px;
+  }
+
+  .ant-table-thead > tr > th {
+    font-weight: bold;
+    background-color: black;
+    color: #ffff;
+    font-size: 12px;
+  }
+
+  .ant-table-thead .ant-table-cell {
+    text-align: center;
+  }
+
+  .ant-table-thead th.ant-table-column-has-sorters:hover {
+    background-color: #000;
+  }
+`;
+
+export default function Table(props) {
+  const [filterWord, setFilterWord] = useState(null);
+  const [filterData, setFilterData] = useState([]);
+
+  useEffect(() => {
+    if (filterWord) {
+      let filterData = props.dataSource.filter((entry) => {
+        let objKey = Object.keys(entry)
+          .map((key) =>
+            entry[key] !== null &&
+            entry[key] !== true &&
+            entry[key] !== false &&
+            entry[key].toString().includes(filterWord) &&
+            props.columns.some((o) => o.dataIndex)
+              ? key
+              : null
+          )
+          .filter((item) => item);
+
+        if (objKey.length > 0) return entry;
+        return null;
+      });
+
+      setFilterData(filterData);
     }
+  }, [props.columns, props.dataSource, filterWord]);
 
-    .ant-table-thead > tr > th{
-        font-weight: bold;
-        background-color: black;
-        color: #FFFF;
-        font-size: 12px;
-    }
-
-    .ant-table-thead .ant-table-cell{
-        text-align: center;
-    }
-
-    .ant-table-thead th.ant-table-column-has-sorters:hover{
-      background-color: #000;
-    }
-
-`
-
-export default function table(props) {
   return (
-    <TableConponent className="table" rowKey={'id'} dataSource={props.dataSource} columns={props.columns} bordered/>
-  )
+    <>
+      <Input
+        onChange={(e) => {
+          let value = e.target.value;
+          if (value === "") value = null;
+          setFilterWord(value);
+        }}
+      />
+      <TableComponent
+        className="table"
+        rowKey={"id"}
+        dataSource={filterWord ? filterData : props.dataSource}
+        columns={props.columns}
+        bordered
+      />
+    </>
+  );
 }
